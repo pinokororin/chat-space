27c912762cae4f4459bb184d3ccb97378837e5d6
pinokororin <pinopinetree3.5@gmail.com>
2019-12-13 19:45:23 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -23,10 +23,8 @@ $(function(){
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    console.log(this)
     var formData = new FormData(this);
     var url = $(this).attr('action')
-    console.log(url)
     $.ajax({
       url: url,
       type: "POST",
